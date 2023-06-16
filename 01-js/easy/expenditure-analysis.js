432ba808461bae0378d02d42d8a35d11774e2537
Suryansh Rana <136776475+suryansh-rana@users.noreply.github.com>
2023-06-16 10:02:50 +0530

    Update expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -8,8 +8,32 @@
   - `npm run test-expenditure-analysis`
 */
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+function calculateTotalSpentByCategory(transactions) {}
+  var categoryMap = {};
+  transactions.forEach(function(transaction) {
+    var category = transaction.category;
+    var price = transaction.price;
+    if (categoryMap.hasOwnProperty(category)) {
+      categoryMap[category] += price;
+    } else {
+      categoryMap[category] = price;
+    }
+  });
+  var result = Object.keys(categoryMap).map(function(category) {
+    var totalSpent = categoryMap[category];
+    var categoryObject = {};
+    categoryObject[category] = totalSpent;
+    return categoryObject;
+  });
+  return result;
 }
-
+var transactions = [
+  { itemName: "keyboard", category: "electronics", price: 1500, timestamp: "2023-06-16" },
+  { itemName: "pen", category: "stationary", price: 15, timestamp: "2023-06-16" },
+  { itemName: "book", category: "stationary", price: 200, timestamp: "2023-06-16" },
+  { itemName: "tshirt", category: "clothes", price: 2500, timestamp: "2023-06-16" },
+  { itemName: "jean", category: "clothes", price: 3000, timestamp: "2023-06-16" }
+];
+var totalSpentByCategory = calculateTotalSpentByCategory(transactions);
+console.log(totalSpentByCategory);
 module.exports = calculateTotalSpentByCategory;
