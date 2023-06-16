9f24e3180782d145a8e6ff98ef2a4259b129935b
Suryansh Rana <136776475+suryansh-rana@users.noreply.github.com>
2023-06-16 09:57:48 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,28 @@
   - `npm run test-anagram`
 */
 
-function isAnagram(str1, str2) {
-
+function isAnagram(str1, str2) {}
+  str1 = str1.replace(/\s/g, '').toLowerCase();
+  str2 = str2.replace(/\s/g, '').toLowerCase();
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  var frequencyMap1 = {};
+  var frequencyMap2 = {};
+  for (var i = 0; i < str1.length; i++) {
+    var char1 = str1[i];
+    var char2 = str2[i];
+    frequencyMap1[char1] = (frequencyMap1[char1] || 0) + 1;
+    frequencyMap2[char2] = (frequencyMap2[char2] || 0) + 1;
+  }
+  for (var char in frequencyMap1) {
+    if (frequencyMap1[char] !== frequencyMap2[char]) {
+      return false;
+    }
+  }
+  return true;
 }
-
+console.log(isAnagram("spar", "rasp")); 
+console.log(isAnagram("hello", "bye"));
+console.log(isAnagram("suryansh", "yashurns")); 
 module.exports = isAnagram;
